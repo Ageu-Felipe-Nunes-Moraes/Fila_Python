70b660b4e9dd4c3241b34aed9803c97d154c7bc1
Ageu Moraes <ageumoraes67@gmail.com>
2024-05-05 11:43:27 -0300

    Renomeei variáveis e funções para inglês

--- Fila/Fila.py
+++ Fila/Fila.py
@@ -1,43 +1,43 @@
 
 import time as t
 
-class Fila:
+class Queue:
     def __init__(self):
-        self.lista = []
+        self.list = []
 
-    def vazia(self):
-        return len(self.lista) == 0
+    def empty(self):
+        return len(self.list) == 0
     
     def push(self, item):
-        self.lista.append(item)
+        self.list.append(item)
     
     def pop(self):
-        if not self.vazia():
-            del(self.lista[0])
+        if not self.empty():
+            del(self.list[0])
         else:
             raise IndexError("A lista está vazia")
         
     def top(self):
-        if not self.vazia():
-            return self.lista[0]
+        if not self.empty():
+            return self.list[0]
         else:
             return None
 
 
-fila = Fila()
+queue = Queue()
 
 for i in range(100+1):
-    fila.push(i)
-    print(f"Lista: {fila.lista}\n")
+    queue.push(i)
+    print(f"Lista: {queue.list}\n")
     t.sleep(0.2)
 
-print(f"Lista preenchida: {fila.lista}\n")
+print(f"Lista preenchida: {queue.list}\n")
 print("Aguarde...\n")
 t.sleep(3)
 
-while not fila.vazia():
-    primeiro_item_lista = fila.top()
-    fila.pop()
-    print(f"Item removido da lista com sucesso: {primeiro_item_lista}\n")
-    print(f"Lista: {fila.lista}\n")
+while not queue.empty():
+    queue_first_item  = queue.top()
+    queue.pop()
+    print(f"Item removido da lista com sucesso: {queue_first_item}\n")
+    print(f"Lista: {queue.list}\n")
     t.sleep(0.2)
